60c041615122cb879a567339dbd68c90d0a3cda9
juju125 <justin555@live.nl>
2021-04-02 11:32:25 +0200

    Added a backend project and started working on test api

---
+++ JavaBackEnd/src/com/company/PersonServlet.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.io.IOException;
+
+import java.lang.Object;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//issue with not finding the import, Maby looking for an alternative
+import org.json.JSONObject;
+
+public class PersonServlet extends HttpServlet {
+}
