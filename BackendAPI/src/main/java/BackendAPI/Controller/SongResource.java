91b41d87ee5e59fad77bfefbd452da50d7aabc44
juju125 <justin555@live.nl>
2021-04-22 12:32:20 +0200

    Fixed some issues with maven but not all

--- BackendAPI/src/main/java/BackendAPI/Controller/SongResource.java
+++ BackendAPI/src/main/java/BackendAPI/Controller/SongResource.java
@@ -4,10 +4,10 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Set;
 
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+//import javax.ws.rs.DELETE;
+//import javax.ws.rs.GET;
+//import javax.ws.rs.POST;
+//import javax.ws.rs.Path;
 
 public class SongResource {
 }
